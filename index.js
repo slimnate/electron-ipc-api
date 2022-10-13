8ceb2edc83fff31be9d54535482463a656d75f1b
Slim Nate <slimnatekc@gmail.com>
2022-10-13 15:01:34 -0500

    implement registerIpcHandlers function and tests

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ function configure(config) {
     //TODO
 }
 
-function registerIpcHandlers() {
-    //TODO
+/**
+ * Register IPC event handlers for the given `api` object.
+ * 
+ * @param {*} ipcMain electron ipcMain module
+ * @param {*} api object containing the api namespaces and methods to be registered.
+ */
+function registerIpcHandlers(ipcMain, api) {
+    Object.keys(api).forEach((prefix) => {
+        // ignore base level functions (like configure)
+        if (typeof api[prefix] === 'function') {
+            return;
+        }
+        // Iterate keys of
+        Object.keys(api[prefix]).forEach((funcName) => {
+            console.log(`registering handler for ${prefix}:${funcName}`)
+            ipcMain.handle(`${prefix}:${funcName}`, api[prefix][funcName]);
+        })
+    });
 }
 
 function getIpcInvoker() {
--- index.test.js
+++ index.test.js
@@ -1,6 +1,19 @@
 const index = require('./index');
 
-describe('Module', () => {
+const testAPI = {
+    nonRegisteredProperty: '',
+    nonRegisteredFunc: () => { /* This function should not be exported */ },
+    namespaceOne: {
+        registeredFuncOne: () => { },
+        registeredFuncTwo: () => { }
+    },
+    namespaceTwo: {
+        registeredFuncOne: () => { },
+        registeredFuncTwo: () => { }
+    },
+}
+
+describe('Module exports', () => {
     test('it should export method - registerIpcHandlers', () => {
         expect(index.registerIpcHandlers).toBeInstanceOf(Function);
     })
@@ -11,5 +24,33 @@ describe('Module', () => {
 
     test('it should export method - configure', () => {
         expect(index.configure).toBeInstanceOf(Function);
+    })
+})
+
+describe('registerIpcHandlers', () => {
+    const ipcMain = {
+        handle: jest.fn()
+    }
+
+    index.registerIpcHandlers(ipcMain, testAPI);
+
+    test('it should not register base level properties', () => {
+        expect(ipcMain.handle).not.toHaveBeenCalledWith('nonRegisteredProperty', testAPI.nonRegisteredProperty);
+    })
+
+    test('it should not register base level functions', () => {
+        expect(ipcMain.handle).not.toHaveBeenCalledWith('nonRegisteredFunc', testAPI.nonRegisteredFunc);
+    })
+
+    test('it should register namespaced functions', () => {
+        expect(ipcMain.handle).toHaveBeenCalledTimes(4)
+        expect(ipcMain.handle).toHaveBeenCalledWith(
+            'namespaceOne:registeredFuncOne', testAPI.namespaceOne.registeredFuncOne);
+        expect(ipcMain.handle).toHaveBeenCalledWith(
+            'namespaceOne:registeredFuncTwo', testAPI.namespaceOne.registeredFuncTwo);
+        expect(ipcMain.handle).toHaveBeenCalledWith(
+            'namespaceTwo:registeredFuncOne', testAPI.namespaceTwo.registeredFuncOne);
+        expect(ipcMain.handle).toHaveBeenCalledWith(
+            'namespaceTwo:registeredFuncTwo', testAPI.namespaceTwo.registeredFuncTwo);
     })
 })
