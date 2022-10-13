7e4a6262606a14489a717b2491339afa42fa0250
Slim Nate <slimnatekc@gmail.com>
2022-10-13 15:49:58 -0500

    implement getIpcInvoker method and tests

--- index.js
+++ index.js
@@ -26,8 +26,22 @@ function registerIpcHandlers(ipcMain, api) {
     });
 }
 
-function getIpcInvoker() {
-    //TODO
+function getIpcInvoker(ipcRenderer, api) {
+    let invoker = {};
+    Object.keys(api)
+    .filter(prefix => {
+        // ignore base level functions (like configure)
+        return typeof api[prefix] === 'object';
+    })
+    .forEach(prefix => {
+        let subModule = {};
+        Object.keys(api[prefix]).forEach(funcName => {
+            console.log(`registering invoker for ${prefix}:${funcName}`)
+            subModule[funcName] = (props) => ipcRenderer.invoke(`${prefix}:${funcName}`, props);
+        })
+        invoker[prefix] = subModule;
+    })
+    return invoker;
 }
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -42,7 +42,7 @@ describe('registerIpcHandlers', () => {
         expect(ipcMain.handle).not.toHaveBeenCalledWith('nonRegisteredFunc', testAPI.nonRegisteredFunc);
     })
 
-    test('it should register namespaced functions', () => {
+    test('it should register namespaced functions by calling ipcMain.handle with correct ipc name', () => {
         expect(ipcMain.handle).toHaveBeenCalledTimes(4)
         expect(ipcMain.handle).toHaveBeenCalledWith(
             'namespaceOne:registeredFuncOne', testAPI.namespaceOne.registeredFuncOne);
@@ -52,5 +52,46 @@ describe('registerIpcHandlers', () => {
             'namespaceTwo:registeredFuncOne', testAPI.namespaceTwo.registeredFuncOne);
         expect(ipcMain.handle).toHaveBeenCalledWith(
             'namespaceTwo:registeredFuncTwo', testAPI.namespaceTwo.registeredFuncTwo);
+    })
+})
+
+describe('getIpcInvoker', () => {
+    const ipcRenderer = {
+        invoke: jest.fn()
+    }
+    const invoker = index.getIpcInvoker(ipcRenderer, testAPI);
+
+    test('it should not contain base level properties', () => {
+        expect(invoker).not.toHaveProperty('nonRegisteredProperty')
+    })
+
+    test('it should not contain base level functions', () => {
+        expect(invoker).not.toHaveProperty('nonRegisteredFunc')
+    })
+
+    test('it should contain invoker methods for every namespaced method in the correct format', () => {
+        expect(invoker).toHaveProperty('namespaceOne.registeredFuncOne', expect.any(Function))
+        expect(invoker).toHaveProperty('namespaceOne.registeredFuncTwo', expect.any(Function))
+        expect(invoker).toHaveProperty('namespaceTwo.registeredFuncOne', expect.any(Function))
+        expect(invoker).toHaveProperty('namespaceTwo.registeredFuncTwo', expect.any(Function))
+    })
+
+    test('it should call ipcRenderer.invoke with correct arguments when invoker methods are called', () => {
+        const testParams=[
+            5,
+            'test param',
+            { test: 'param'},
+            [1,2,3,4,5],
+        ]
+        invoker.namespaceOne.registeredFuncOne(testParams[0])
+        invoker.namespaceOne.registeredFuncTwo(testParams[1])
+        invoker.namespaceTwo.registeredFuncOne(testParams[2])
+        invoker.namespaceTwo.registeredFuncTwo(testParams[3])
+
+        expect(ipcRenderer.invoke).toHaveBeenCalledTimes(4)
+        expect(ipcRenderer.invoke).toHaveBeenCalledWith('namespaceOne:registeredFuncOne', testParams[0])
+        expect(ipcRenderer.invoke).toHaveBeenCalledWith('namespaceOne:registeredFuncTwo', testParams[1])
+        expect(ipcRenderer.invoke).toHaveBeenCalledWith('namespaceTwo:registeredFuncOne', testParams[2])
+        expect(ipcRenderer.invoke).toHaveBeenCalledWith('namespaceTwo:registeredFuncTwo', testParams[3])
     })
 })
